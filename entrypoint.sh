7e76ea94eeacf210726f3f1ed4f5f1c792e30b23
Robert <robert.warmack@gmail.com>
2018-09-10 12:22:19 -0700

    The issue with MISSING isn't kube-specific.

--- entrypoint.sh
+++ entrypoint.sh
@@ -76,16 +76,12 @@ done
 
 echo "}" >> cluster_dump.tmp
 
-# Kube dumps some weird non-JSON values to indicate (MISSING) which breaks
-#   our future processing.
-perl -pi -e 's/\(MISSING\)//g' cluster_dump.tmp 
-
 cat cluster_dump.tmp | jq type
 if [ $? -ne 0 ]; then
   echo "Scan has created invalid JSON."
   exit 1
 else
-  echo "Cluster JSON is valid!"
+  echo "Cluster JSON is valid."
 fi
 
 echo "$(date -u): Creating shared secrets for encryption."
